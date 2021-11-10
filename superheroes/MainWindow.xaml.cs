3c9f909b400fd428df116f558cd0cd3264628a4d
JAcutillas <ja.cutillas@iesdoctorbalmis.com>
2021-11-10 10:00:18 +0100

    En principio terminada

--- superheroes/MainWindow.xaml.cs
+++ superheroes/MainWindow.xaml.cs
@@ -20,13 +20,36 @@ namespace superheroes
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int posicionArray = 0;
+        List<Superheroe> listaHeroes;
         public MainWindow()
         {
             InitializeComponent();
 
-            List<Superheroe> listaHeroes = Superheroe.GetSamples();
+            listaHeroes = Superheroe.GetSamples();
 
-            principalBorder.DataContext = listaHeroes[1];
+            principalBorder.DataContext = listaHeroes[posicionArray];
+        }
+
+        private void MasClick(object sender, MouseButtonEventArgs e)
+        {
+            if (posicionArray < 2)
+            {
+                posicionArray++;
+                principalBorder.DataContext = listaHeroes[posicionArray];
+                PosicionTextBlock.Text = (posicionArray + 1).ToString() + "/3"; 
+            }
+        }
+
+        private void MenosClick(object sender, MouseButtonEventArgs e)
+        {
+            if (posicionArray > 0)
+            {
+                posicionArray--;
+                principalBorder.DataContext = listaHeroes[posicionArray];
+                PosicionTextBlock.Text = (posicionArray + 1).ToString() + "/3";
+            }
         }
     }
 }
